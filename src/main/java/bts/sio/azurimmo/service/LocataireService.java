da561b0ced8faa892a288009be17db27520608ba
benedicte.gady <benedicte.gady@sts-sio-caen.info>
2025-05-29 11:44:16 +0200

    Modif LocataireService pour addLocataire

--- src/main/java/bts/sio/azurimmo/service/LocataireService.java
+++ src/main/java/bts/sio/azurimmo/service/LocataireService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import bts.sio.azurimmo.model.Contrat;
 import bts.sio.azurimmo.model.Locataire;
 import bts.sio.azurimmo.repository.LocataireRepository;
 import lombok.Data;
@@ -13,11 +14,15 @@ import lombok.Data;
 public class LocataireService {
 	@Autowired
 	private LocataireRepository locataireRepository;
-
+	
 	public Locataire saveLocataire(Locataire locataire) {
+		if (locataire.getId() == 0) {
+			locataire.setId(null);
+        }
 		Locataire savedLocataire = locataireRepository.save(locataire);
 		return savedLocataire;
 	}
+
 
 	public List<Locataire> findAll() {
 		return locataireRepository.findAll();
